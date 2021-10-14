cd1e29af5c1222b72fe0a5c57f7f1ac1806be4a5
kz2d <avidex111@gmail.com>
2021-10-14 18:28:58 +0300

    index js

---
+++ index.js
@@ -0,0 +1,45 @@
+module.exports = function (Homework) {
+  let promisify =
+    (fn) =>
+    (...args) =>
+      new Promise((resolve, reject) => {
+        fn(...args, (r) => {
+          resolve(r);
+        });
+      });
+
+  const {
+    AsyncArray,
+    add,
+    subtract,
+    multiply,
+    divide,
+    less,
+    equal,
+    lessOrEqual,
+  } = Homework;
+
+  return (asyncArray, fn, initialValue, cb) => {
+    let main = async () => {
+      let res = initialValue;
+      let newFn = promisify(fn);
+      let newGet = promisify(asyncArray.get);
+      let newLength = promisify(asyncArray.length);
+      let newLess = promisify(less);
+      try {
+        let length = 0;
+        length = await newLength();
+        console.log("length=" + length);
+        for (let i = 0; await newLess(i, length); i++) {
+          console.log(res);
+
+          res = await newFn(res, await newGet(i), i, asyncArray);
+        }
+      } catch (e) {
+        console.log("error=" + e);
+      }
+      return res;
+    };
+    main().then(cb);
+  };
+};
