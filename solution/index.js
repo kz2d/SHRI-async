2741bd739a928493e9c5c55375ccb654c8aed4db
kz2d <avidex111@gmail.com>
2021-10-17 17:26:44 +0300

    add prettier

--- solution/index.js
+++ solution/index.js
@@ -1,38 +1,30 @@
 module.exports = function (Homework) {
-  const {
-    AsyncArray,
-    add,
-    subtract,
-    multiply,
-    divide,
-    less,
-    equal,
-    lessOrEqual,
-  } = Homework;
+const {AsyncArray, add, subtract, multiply, divide, less, equal, lessOrEqual} =
+Homework
 
-  return (asyncArray, fn, initialValue, cb) => {
-    let res = initialValue;
-    let counter = 0;
+return (asyncArray, fn, initialValue, cb) => {
+let res = initialValue
+let counter = 0
 
-    asyncArray.length((length) => {
-      let rFn = (some) => {
-        res = some;
-        add(counter, 1, (r) => {
-          counter = r;
-          equal(counter, length, (r) => {
-            if (counter == length) {
-              cb(res);
-            } else {
-              asyncArray.get(counter, (val) => {
-                fn(res, val, counter, asyncArray, rFn);
-              });
-            }
-          });
-        });
-      };
-      asyncArray.get(counter, (val) => {
-        fn(res, val, counter, asyncArray, rFn);
-      });
-    });
-  };
-};
+asyncArray.length((length) => {
+let rFn = (some) => {
+res = some
+add(counter, 1, (r) => {
+counter = r
+equal(counter, length, (r) => {
+if (counter == length) {
+cb(res)
+} else {
+asyncArray.get(counter, (val) => {
+fn(res, val, counter, asyncArray, rFn)
+})
+}
+})
+})
+}
+asyncArray.get(counter, (val) => {
+fn(res, val, counter, asyncArray, rFn)
+})
+})
+}
+}
